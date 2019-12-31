9bb9ef81f9297263d8ce51f027e537ca340c42f3
Chloe Kudryavtsev <toast@toast.cafe>
2019-12-31 13:37:43 -0500

    move to mcli because plugins/s3 is semi-broken
    also see: rclone

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -z "$s3_id" ] || [ -z "$s3_key" ] || [ -z "$s3_endpoint" ]; then
+    echo 's3_id, s3_key and s3_endpoint environment variables are mandatory'
+    return 1
+fi
+
+MCLI=mcli
+[ $# -ge 1 ] && MCLI="$1"
+
+export MC_HOST_target="https://$s3_id:$s3_key@$s3_endpoint"
+$MCLI cp abyss.tar.gz "target/$s3_id/lxd/"
