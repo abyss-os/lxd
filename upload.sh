72117bd8eedd314197c5401a1dad2e0731c213f6
Joe Holden <jwh@zorins.us>
2020-01-27 08:58:12 +0000

    minio/mc isn't multi-arch, use minio-client package

--- upload.sh
+++ upload.sh
@@ -5,17 +5,4 @@ case $DRONE_STAGE_ARCH in
         *) echo "unknown arch" ; exit 1;;
 esac
 
-if [ -z "$s3_id" ] || [ -z "$s3_key" ] || [ -z "$s3_endpoint" ]; then
-    echo 's3_id, s3_key and s3_endpoint environment variables are mandatory'
-    return 1
-fi
-
-MCLI=mcli
-[ $# -ge 1 ] && MCLI="$1"
-
-if [ "${buildarch}" = "x86_64" ]; then
-    export MC_HOST_target="https://$s3_id:$s3_key@$s3_endpoint"
-    $MCLI cp abyss.tar.gz "target/$s3_id/lxd/"
-fi
-
-$MCLI cp abyss.tar.gz "master/abyss-ci/lxd-snap-${buildarch}-$(date +%Y%m%d).tgz"
+mcli cp abyss.tar.gz "master/abyss-ci/lxd-snap-${buildarch}-$(date +%Y%m%d).tgz"
