6e43f0b4d65e8396882940f143b046d421ea6a51
Joe Holden <jwh@zorins.us>
2020-06-04 21:41:53 +0000

    add ppc64le and mips64

--- upload.sh
+++ upload.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 case $DRONE_STAGE_ARCH in
-        amd64) buildarch=x86_64;;
-        arm64) buildarch=aarch64;;
-        *) echo "unknown arch" ; exit 1;;
+	amd64) buildarch=x86_64;;
+	arm64) buildarch=aarch64;;
+	ppc64le) buildarch=ppc64le;;
+	mips64) buildarch=mips64;;
+	*) echo "unknown arch" ; exit 1;;
 esac
 
 mcli cp --attr arch="${buildarch}"\;stamp="$(date +%Y%m%d)" abyss.tar.gz "master/abyss-ci/lxd/snap-${buildarch}-$(date +%Y%m%d).tgz"
