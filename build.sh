d889ded56a9e58b5e16b101911d8d48510ef0769
Joe Holden <jwh@zorins.us>
2020-06-16 14:05:12 +0000

    broken

--- build.sh
+++ build.sh
@@ -11,6 +11,20 @@ ARCH=$(apk --print-arch)
 ABYSS_CORE=https://mirror.abyss.run/abyss/core
 ABYSS_DEV=https://mirror.abyss.run/abyss/devel
 
+case "$ARCH" in
+	mips64*|riscv64) cc=none binutils=gnu;;
+	*)	cc=none binutils=llvm;;
+esac
+
+# chroot inside rootfs
+crun() {
+    chroot rootfs "$@"
+}
+# create base rootfs
+apka() {
+    apk add -X "$ABYSS_CORE" --no-cache --allow-untrusted --initdb --root rootfs "$@"
+}
+
 # get prereqs
 apk add bsdtar squashfs-tools
 
@@ -23,10 +37,6 @@ chmod +x esh
 # generate metadata
 ./esh -o metadata.yaml metadata.yaml.esh
 
-# create base rootfs
-apka() {
-    apk add -X "$ABYSS_CORE" --no-cache --allow-untrusted --initdb --root rootfs "$@"
-}
 apka abyss-base
 echo "$ABYSS_CORE" > rootfs/etc/apk/repositories
 echo "$ABYSS_DEV" >> rootfs/etc/apk/repositories
@@ -34,10 +44,6 @@ echo "$ABYSS_DEV" >> rootfs/etc/apk/repositories
 # prep for chroot
 cp /etc/resolv.conf rootfs/etc/
 
-# chroot inside rootfs
-crun() {
-    chroot rootfs "$@"
-}
 # enable services
 crun ln -s /etc/init.d/getty /etc/init.d/getty.console
 crun rc-update add getty.console default
@@ -48,23 +54,25 @@ crun rc-update add loopback sysinit
 crun sed -i 's/^persistent/#persistent/' /etc/dhcpcd.conf
 
 # create unified tarball
+echo "latest.tar.gz"
 bsdtar -caf latest.tar.gz rootfs metadata.yaml
 
+stat latest.tar.gz
 # create separate images + squashfs
 #bsdtar -caf latest.tar.gz metadata.yaml
 #mksquashfs rootfs rootfs.squashfs -all-root
 
+rm -vf latest.tar.gz metadata.yaml rootfs.squashfs
+
 # now make dev
 crun apk add abyss-base-dev
 crun ln -s /usr/bin/gmake /usr/bin/make
-case "$ARCH" in
-	mips64*|riscv64) cc=none binutils=gnu;;
-	*)	cc=none binutils=llvm;;
-esac
 crun toolchain $cc $binutils
 crun adduser root abuild
 
 ./esh -o metadata.yaml metadata.dev.yaml.esh
 
 # create unified tarball
+echo "dev.tar.gz"
 bsdtar -caf dev.tar.gz rootfs metadata.yaml
+stat latest.tar.gz
